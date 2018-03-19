3f80e2f17ef59857859d4d42300005ff25cb6e28
Ajay Hemnani <ajaych@google.com>
2018-03-19 13:53:13 -0700

    added Filter.java

--- create_mvn.sh
+++ create_mvn.sh
@@ -1,7 +1,7 @@
 mvn archetype:generate \
   -DarchetypeArtifactId=google-cloud-dataflow-java-archetypes-starter \
   -DarchetypeGroupId=com.google.cloud.dataflow \
-  -DgroupId=com.google.cloud.training.dataanalyst.javahelp \
+  -DgroupId=com.google.cloud.training.logprocessor \
   -DartifactId=javahelp2 \
   -Dversion="[1.0.0,2.0.0]" \
   -DinteractiveMode=false \
